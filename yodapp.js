1b9a25c8e874b970143f241e4a2c5539f19b92b7
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2021-04-13 12:54:43 +0530

    js connection working

---
+++ yodapp.js
@@ -0,0 +1,10 @@
+// taking input
+var secretId = prompt("Do You Want To Begin?");
+
+
+//  processing
+var secretMessage = "Handshake 100% - JS Connection Made, " + secretId;
+
+
+// output
+alert(secretMessage);
