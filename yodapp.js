93d80867f123de723791d4a1a9fb6aa88a292695
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2021-04-13 13:06:03 +0530

    completed

--- yodapp.js
+++ yodapp.js
@@ -1,10 +1,32 @@
+// get me these elements
+var btnClick = document.querySelector("#tapme");
+var txt = document.querySelector("#textArea");
+var div = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/yoda.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+// error message 
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Wrong with server, something! Again after some time, try.")
+}
+
 // taking input
-var secretId = prompt("Do You Want To Begin?");
+function clickHandler() {
+    var inputText = txt.value; 
 
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            div.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+};
 
-//  processing
-var secretMessage = "Handshake 100% - JS Connection Made, " + secretId;
-
-
-// output
-alert(secretMessage);
+btnClick.addEventListener("click", clickHandler)
